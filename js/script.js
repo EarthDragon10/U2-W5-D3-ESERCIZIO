5668ecbcdeac4264dbc0def9eb89f691264b3faa
EarthDragon10 <lagudadev@gmail.com>
2022-09-28 18:18:18 +0200

    added js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let n_img = 10;
 let corrente = 1;
 
+let images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const nextImg = () => {
 	document.querySelector(`.slides:nth-child(${corrente})`).style.display =
 		"none";
@@ -8,14 +10,23 @@ const nextImg = () => {
 	document.querySelector(`.slides:nth-child(${corrente + 5})`).style.display =
 		"block";
 	document
-		.querySelector(`.slides:nth-child(${corrente + 5})`)
+		.querySelector(`.slides:nth-child(${corrente + 3})`)
 		.classList.toggle("images-animation");
 	corrente++;
 	console.log(corrente);
+
+	if (corrente > 6) {
+		document.querySelector(`.slides:nth-child(4)`).style.display = "none";
+		document.querySelector(`.slides:nth-child(1)`).style.display = "block";
+		// corrente = 1;
+	}
 };
 
 const playSlider = () => {
-	setInterval(nextImg, 1000);
+	return setInterval(nextImg, 1000);
 };
 
+const stopSlider = () => {
+	clearInterval(playSlider);
+};
 console.log(corrente);
